d0153512f34247322d1373d62e76d6767b77dbe3
swoolard42 <sw10701@cabrini.edu>
2022-12-08 12:13:19 -0500

    file cleanup

--- javascript.js
+++ javascript.js
@@ -12,22 +12,3 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
-
-
-
-{/* <script src="/path/to/flickity.pkgd.min.js"></script>
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'center',
-  contain: true,
-  autoPlay: true,
-  wrapAround: true,
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-}); */}
